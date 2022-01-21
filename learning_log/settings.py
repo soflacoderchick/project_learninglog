67ff04822558bb7d12f26f4b6c732c0ed3b99c6d
Jana <jana.panarites@gmail.com>
2022-01-21 17:23:15 -0500

    Ready for deployment to heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -135,6 +135,7 @@ LOGIN_URL = 'users:login'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
